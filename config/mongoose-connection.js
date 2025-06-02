fe73f0c5f9a40026c293c57986438f8a2e035f7e
QubeBrowser <qubebrower@gmail.com>
2025-06-03 01:25:17 +0530

    backend viedo 3 done

---
+++ config/mongoose-connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Scatch').then(function () {
+    console.log('Connected Successfully');
+}).catch(function (err) {
+    console.log(err);
+})
+
+module.exports = mongoose.connection ; 
+
