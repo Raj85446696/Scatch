69c9532ef291dc2237ed665a37ffe24aea046251
QubeBrowser <qubebrower@gmail.com>
2025-06-14 15:10:30 +0530

    done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/userModel');
+const productModel = require('../models/productModel');
 const bcrypt = require('bcrypt');
 const { generateToken } = require('../utils/generateToken');
 module.exports.registerUser = async (req, res) => {
@@ -28,22 +29,30 @@ module.exports.registerUser = async (req, res) => {
 }
 
 module.exports.loginUser = async (req, res) => {
-    let { email, password } = req.body;
-    let user = await userModel.findOne({ email: email });
-    if (!user) return res.send('Email or Password is incorrect');
-    bcrypt.compare(password, user.password, (err, result) => {
-        if (result) {
-            let token = generateToken(user);
-            res.cookie('token', token);
-            res.send('You are in shop page');
-            // res.render('shop');
-        } else {
-            res.send('Email or Password is incorrect')
-        }
-    });
-}
+    try {
+        let { email, password } = req.body;
+        let user = await userModel.findOne({ email: email });
+        if (!user) return res.send('Email or Password is incorrect');
 
-module.exports.logoutUser = async (req,res)=>{
-    res.cookie('token','');
+        const result = await bcrypt.compare(password, user.password);
+        if (!result) return res.send('Email or Password is incorrect');
+
+        let token = generateToken(user);
+        res.cookie('token', token);
+
+        const products = await productModel.find();
+
+        if (!products) return res.send('No products found');
+
+        res.render('shop', { products,isLoggedIn:true }); 
+    } catch (err) {
+        console.error('Login error:', err);
+        res.status(500).send('Server error');
+    }
+};
+
+
+module.exports.logoutUser = async (req, res) => {
+    res.cookie('token', '');
     res.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,14 @@ const isLoggedIn = require('../middlewares/isLoggedIn');
 router.get('/',(req,res)=>{
     let error = req.flash('error');
     res.render('index',{error});
-})
+});
 
 router.get('/shop',isLoggedIn,(req,res)=>{
     res.render('shop');
+});
+
+router.get('/addtocart/:id',isLoggedIn,async(req,res)=>{
+    const user = await userModel.findOne({user:req.user.email});
 })
 
 
